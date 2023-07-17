622f2afb225418c61a0cbe1ae1e5c5452a46818e
shivhub09 <shivnagori2020@gmail.com>
2023-07-17 14:17:31 +0530

    change of files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:netflix_clone/getstarted.dart';
+import 'package:netflix_clone/screens/getstarted.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
